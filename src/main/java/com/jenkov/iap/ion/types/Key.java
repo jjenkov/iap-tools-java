5bf86aea32c56687d902c611f0ad450ec06c9d65
Jakob Jenkov <jakob@jenkov.com>
2016-04-12 17:51:28 +0200

    Convenience constructors added

--- src/main/java/com/jenkov/iap/ion/types/Key.java
+++ src/main/java/com/jenkov/iap/ion/types/Key.java
@@ -1,5 +1,7 @@
 package com.jenkov.iap.ion.types;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by jjenkov on 12-04-2016.
  */
@@ -7,4 +9,30 @@ public class Key {
     public byte[] source = null;
     public int    offset = 0;
     public int    length = 0;
+
+    public Key() {
+    }
+
+    public Key(byte[] source) {
+        this.source = source;
+        this.offset = 0;
+        this.length = source.length;
+    }
+
+    public Key(byte[] source, int offset, int length) {
+        this.source = source;
+        this.offset = offset;
+        this.length = length;
+    }
+
+    public Key(String key){
+        try {
+            this.source = key.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        this.offset = 0;
+        this.length = this.source.length;
+    }
 }
